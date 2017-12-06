fe4427628a6acbbc2cd2c6d14dae7c8505baee2d
Maanu449 <mansi10022000@gmail.com>
2017-12-07 02:45:04 +0530

    final code

--- app/src/main/java/com/example/android/sqlite/MainActivity.java
+++ app/src/main/java/com/example/android/sqlite/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.android.sqlite;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import java.util.List;
-
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,18 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        sqlOperations1 db = new sqlOperations1(this);
+        Log.d("List" , "Listing");
+
+        db.addEntry(new Names("ABC"));
+
+        List<Names> list = db.GetAllEntries();
+        for(Names nm: list){
+            String display = "Roll no. : " + nm.getId() + " Name: " + nm.getname();
+            Log.d("Entry ", display);
+        }
     }
-
-    sqlOperations1 db = new sqlOperations1(this);
-    Log.d("List" , "Listing");
-
-    db.addEntry(new Names("ABC"));
-
-    List<Names> list = db.GetAllEntries();
-    for(Names nm: list){
-        String display = "Roll no. : " + nm.getId() + " Name: " + nm.getname();
-    }
-
-    Log.d("Entry " + display);
 
 }
